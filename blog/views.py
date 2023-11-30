9bee3691e250aa93be086c92376da5973f82d2bd
Omid-yaghoobi7 <omid.yaghoobi@yahoo.com>
2023-12-01 01:13:24 +0330

    Template tags continue
    Template tags continue

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,12 @@ from django.http import HttpResponse, JsonResponse
 from blog.models import Post
 
 # Create your views here.
-def blog_view(request):
+def blog_view(request, cat_name=None, author_username=None):
     posts = Post.objects.filter(status=1)
+    if cat_name:
+        posts = posts.filter(category__name=cat_name)
+    if author_username:
+        posts = posts.filter(author__username=author_username)
     context = {'posts': posts}
     return render(request, 'blog/blog-home.html', context)
 
@@ -16,3 +20,11 @@ def blog_single(request, pid):
 
 def test(request):
     return render(request, 'test.html')
+
+def blog_search(request):
+    posts = Post.objects.filter(status=1)
+    if request.method == 'GET':
+        if search_get := request.GET.get('s'):
+            posts = posts.filter(content__contains=search_get)
+    context = {'posts': posts}
+    return render(request, 'blog/blog-home.html', context)
