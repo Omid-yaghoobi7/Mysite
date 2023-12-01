0edfd4fe9d24b76610fe35be6d9be7feaade23f8
Omid-yaghoobi7 <omid.yaghoobi@yahoo.com>
2023-12-02 01:00:40 +0330

    template tags continue
    template tags continue

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, JsonResponse
 from blog.models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 def blog_view(request, cat_name=None, author_username=None):
@@ -9,8 +10,20 @@ def blog_view(request, cat_name=None, author_username=None):
         posts = posts.filter(category__name=cat_name)
     if author_username:
         posts = posts.filter(author__username=author_username)
+
+    posts = Paginator(posts, 3)
+    
+    try:
+        page_number = request.GET.get('page')
+        posts = posts.get_page(page_number)
+    except PageNotAnInteger:
+        posts = posts.get_page(1)
+    except EmptyPage:
+        posts = posts.get_page(1)
+
     context = {'posts': posts}
     return render(request, 'blog/blog-home.html', context)
+
 
 def blog_single(request, pid):
     posts = Post.objects.filter(status=1)
