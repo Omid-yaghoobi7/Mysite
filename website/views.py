3d5d9d58785cedb94c4f3c5cf7326dafdc61f666
Omid-yaghoobi7 <omid.yaghoobi@yahoo.com>
2023-12-03 01:54:08 +0330

    Creating forms
    Creating forms

--- website/views.py
+++ website/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from website.models import Contact
+from website.forms import NameForm, ContactForm
+
 
 def index_view(request):
     return render(request, 'webtemp/index.html')
@@ -9,3 +12,15 @@ def about_view(request):
 
 def contact_view(request):
     return render(request, 'webtemp/contact.html')
+
+def test_view(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse('DONE!')
+        else:
+            return HttpResponse('NOT VALID!!!')
+
+    form = ContactForm()
+    return render(request, 'test.html', {'form':form})
