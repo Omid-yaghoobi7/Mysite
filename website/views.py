b8bca3d0c7c406eea3a0befd8bf98107ff36fe07
Omid-yaghoobi7 <omid.yaghoobi@yahoo.com>
2023-12-04 01:40:28 +0330

    Edit forms in template
    Edit forms in template

--- website/views.py
+++ website/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
 from website.models import Contact
-from website.forms import NameForm, ContactForm
+from website.forms import NameForm, ContactForm, NewsletterForm
+from django.contrib import messages
 
 
 def index_view(request):
@@ -11,7 +12,16 @@ def about_view(request):
     return render(request, 'webtemp/about.html')
 
 def contact_view(request):
-    return render(request, 'webtemp/contact.html')
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.add_message(request, messages.SUCCESS, 'Your message has been sent successfully.')
+        else:
+            messages.add_message(request, messages.ERROR, 'There is an error.Try again later.')
+
+    form = ContactForm()
+    return render(request, 'webtemp/contact.html', {'form':form})
 
 def test_view(request):
     if request.method == 'POST':
@@ -24,3 +34,12 @@ def test_view(request):
 
     form = ContactForm()
     return render(request, 'test.html', {'form':form})
+
+def newsletter_view(request):
+    if request.method == 'POST':
+        form = NewsletterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/')
+    else:
+        return HttpResponseRedirect('/')
