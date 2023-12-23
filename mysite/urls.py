bb6de94a009efb8de63aa23bef7298bb40170f06
Omid-yaghoobi7 <omid.yaghoobi@yahoo.com>
2023-12-24 01:31:52 +0330

    Account authentication
    Account authentication

--- mysite/urls.py
+++ mysite/urls.py
@@ -35,6 +35,7 @@ urlpatterns = [
     path("sitemap.xml",sitemap,{"sitemaps": sitemaps},name="django.contrib.sitemaps.views.sitemap",),
     path('robots.txt', include('robots.urls')),
     path("__debug__/", include("debug_toolbar.urls")),
+    path('accounts/', include('accounts.urls')),
     path('summernote/', include('django_summernote.urls')),
     path('captcha/', include('captcha.urls')),
 ]
