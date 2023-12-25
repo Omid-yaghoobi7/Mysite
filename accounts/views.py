20ff6d2d32c09a2e4bf399a66b76c83c9b2ff90a
Omid-yaghoobi7 <omid.yaghoobi@yahoo.com>
2023-12-25 23:31:44 +0330

    User Authentication
    User Authentication

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.urls import reverse
 
 
 # Create your views here.
@@ -22,8 +23,20 @@ def login_view(request):
     else:
         return redirect('/')
 
-#def logout_view(request):
-#    return
+def logout_view(request):
+    if request.user.is_authenticated:
+        logout(request)
+    return redirect('/')
 
 def signup_view(request):
-    return render(request, 'accounts/signup.html')
+    if not request.user.is_authenticated:
+        if request.method == 'POST':
+            form = UserCreationForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return redirect('/')
+        form = UserCreationForm()
+        context = {'form':form}
+        return render(request, 'accounts/signup.html', context)
+    else:
+        return redirect('/')
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
 from blog.models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from blog.forms import CommentForm
 from django.contrib import messages
+from django.urls import reverse
 
 # Create your views here.
 def blog_view(request, cat_name=None, author_username=None, tag_name=None):
@@ -39,10 +40,13 @@ def blog_single(request, pid):
             messages.add_message(request, messages.ERROR, 'There is an error.Try again later.')
     posts = Post.objects.filter(status=1)
     post = get_object_or_404(posts, pk=pid)
-    comments = Comment.objects.filter(post=post.id, approved=True)
-    form = CommentForm()
-    context = {'post': post, 'comments':comments, 'form':form}
-    return render(request, 'blog/blog-single.html', context)
+    if not post.login_require:
+        comments = Comment.objects.filter(post=post.id, approved=True)
+        form = CommentForm()
+        context = {'post': post, 'comments':comments, 'form':form}
+        return render(request, 'blog/blog-single.html', context)
+    else:
+        return HttpResponseRedirect(reverse('accounts:login'))
 
 def test(request):
     return render(request, 'test.html')
